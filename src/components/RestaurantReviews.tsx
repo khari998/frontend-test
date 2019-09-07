b583dd4f946391e69c17e8e0b13af175cde93a6d
Khari <khari.lane@khalikenterprises.com>
2019-09-07 13:39:59 -0500

    Added Map marker with color to match styling, removed typings folder -- redundant code.

--- src/components/RestaurantReviews.tsx
+++ src/components/RestaurantReviews.tsx
@@ -5,7 +5,9 @@ import * as React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Query } from 'react-apollo'
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps'
+
+
 
 import { Restaurant, Review } from '../models/models'
 import { Fragment } from 'react';
@@ -13,53 +15,34 @@ import RestaurantReviewItem from './RestaurantReviewItem';
 import StarRating from './StarRating';
 
 
-const Map = ({ match }: any) => {
-  // Access restaurant that was clicked by matching the id property to the restaruant held in state
-  const selectedRest: Restaurant = useSelector((state: any) => state.restaurants).filter((rest: Restaurant) => rest.resId === match.params.id)[0]
-  
-  return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: selectedRest.location[0], lng: selectedRest.location[1] }}
-    />
-  )
-}
-
-const WrappedMap = withScriptjs(withGoogleMap(Map))
-
-const OutputMap = () => {
-  return (
-    <div style={{ height: '100vw', width: '100vw' }}>
-      <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-      />
-    </div>
-  )
-}
-
 
 export default function RestaurantReviews({ match }: any) { // id property is initialized after initial params so any type is used
 
   // Access restaurant that was clicked by matching the id property to the restaruant held in state
   const selectedRest: Restaurant = useSelector((state: any) => state.restaurants).filter((rest: Restaurant) => rest.resId === match.params.id)[0]
   
+  const restPosition = { lat: selectedRest.location[0], lng: selectedRest.location[1] }
+
   const Map = () => {
-    
     return (
       <GoogleMap
         defaultZoom={18}
-        defaultCenter={{ lat: selectedRest.location[0], lng: selectedRest.location[1] }}
-      />
+        defaultCenter={restPosition}
+      >
+        <Marker
+          position={restPosition}
+          icon={{
+            url: '../assets/MapMarker.png',
+            scaledSize: new google.maps.Size(50, 88)
+          }}
+        ></Marker>
+      </GoogleMap>
     )
   }
 
   const WrappedMap = withScriptjs(withGoogleMap(Map))
 
   const OutputMap = () => {
-    console.log(process.env.REACT_APP_GOOGLE_KEY)
     return (
       <div style={{ height: '100vw', width: '100vw' }}>
         <WrappedMap
@@ -71,22 +54,6 @@ export default function RestaurantReviews({ match }: any) { // id property is in
       </div>
     )
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return (
     <Fragment>
