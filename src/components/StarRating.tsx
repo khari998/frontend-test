8a1ac0f1fb9f0888797ba2cdcebf9d6c89d19eac
Khari <khari.lane@khalikenterprises.com>
2019-09-08 16:42:03 -0500

    Fixed color scheme for Stars

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -11,11 +11,11 @@ export default function StarRating({ rating }: ratingProp) {
   const starArr: any = []; // Assigned to type any due to issue with FontAwesomeIcon type
   for (let i = 0; i < 5; i++) { // Always gives 5 stars, uses i for unique key
     if (rating >= 1) { // Logic for full stars
-      starArr.push(<FontAwesomeIcon icon={['fas', 'star']} key={i} />)
+      starArr.push(<FontAwesomeIcon icon={['fas', 'star']} color="#002B56" key={i} />)
     } else if (rating > 0 && rating < 1) { // Accounts for half stars
-      starArr.push(<FontAwesomeIcon icon="star-half-alt" key={i} />)
+      starArr.push(<FontAwesomeIcon icon="star-half-alt" color="#002B56" key={i} />)
     } else { // Otherwise push empty star
-      starArr.push(<FontAwesomeIcon icon={['far', 'star']} key={i} />)
+      starArr.push(<FontAwesomeIcon icon={['far', 'star']} color="#002B56" key={i} />)
     }
     rating-- // Decrement rating by 1
   }
